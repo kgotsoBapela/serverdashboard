76303aeecb239cec49a7458a1042bd50ec51223a
Kgotso Bapela <kgotso.bapela@rain.co.za>
2024-03-26 15:29:52 +0200

    Repo init, initial dashboard sample draft

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,15 +1,15 @@
-import { Component, input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrl: './cockpit.component.css'
 })
-export class AppComponent {
-  serverElements: any[] = [];
+export class CockpitComponent {
+
   newServerName = '';
   newServerContent = '';
-
+  
   onAddServer() {
     this.serverElements.push({
       type: 'server',
